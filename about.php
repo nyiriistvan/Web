0c293fb58688f4cb99e4a30ad7b8a07d33509df7
nyiriistvan <nyiri.istvan19@gmail.com>
2022-05-26 09:48:47 +0200

    Névjegy és weboldal kész

---
+++ about.php
@@ -0,0 +1,5 @@
+<?php
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/about.html');
+echo file_get_contents('templates/foot.html');
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/home.html');
+echo file_get_contents('templates/foot.html');
